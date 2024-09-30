10513565b5c7f2f1021adae0cfd0c6b75e61af59
Tata-Gav <tata.8web@gmail.com>
2024-09-30 19:07:45 +0300

    7+++

--- find.sh
+++ find.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-echo "Everything that starts with an 'a': "
-find . -type f -name 'a*'
-
-echo "All the files ending with a 'z': "
-find . -type f -name '*z'
-
-echo "All files starting with  All files starting with 'z' and ending with 'a': "
-find . -type f -name 'z*a'
+echo find . \(-name 'a*' -name '*z' -name 'z*a'\)
