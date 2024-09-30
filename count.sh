eb14ed30968e3d5ab5ffda39996a4da19f623ee9
Tata-Gav <tata.8web@gmail.com>
2024-09-30 15:14:00 +0300

    5+

--- count.sh
+++ count.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 # count fils in directory
-count=$(find . -type f -o -type d | wc -l)
-
-# *5
-result=$((count * 5))
-
-# print entr
-printf "\t\tTotal files * 5: %d\v\n" "$result"
+count=$(find . | wc -l); printf "\t\vTotal files * 5: %d\v\n" "$((count * 5))
