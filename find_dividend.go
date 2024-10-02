14b2ea779b2cbedbce50d3c645ab00ab4f56220f
Tata-Gav <tata.8web@gmail.com>
2024-10-02 15:05:59 +0300

    FindDividend

---
+++ find_dividend.go
@@ -0,0 +1,13 @@
+package sprint
+
+func FindDividend(from, to, divisor int) int {
+	if from >= to {
+		return -1
+	}
+	for i := from; i < to; i++ {
+		if i % divisor == 0 {
+			return i
+		}
+	}
+	return -1
+}
