ea8c83ae809a2b5a4986fa6e76068a8961263250
Tata-Gav <tata.8web@gmail.com>
2024-10-03 16:22:33 +0300

    SimpleSTR

---
+++ simple_str_to_int.go
@@ -0,0 +1,12 @@
+package sprint
+
+func SimpleStrToInt(s string) int {
+
+	result := 0
+
+	for i := 0; i < len(s); i++ {
+		digit := int (s[i] - '0')
+		result = result*10 + digit
+	}
+	return result
+}
