22dfbda1af8f3ef9ca9d978c0c16ae8a84be810f
Tata-Gav <tata.8web@gmail.com>
2024-10-03 14:01:43 +0300

    count odwn

--- countdown.go
+++ countdown.go
@@ -3,15 +3,12 @@ package sprint
 func Countdown(n int) string {
 	result := ""
 
-	for i :=n; i >= 0; i -= 2 {
+	for i := n; i >= 0; i -= 2 {
 		if result != "" {
 			result += ", "
 		}
-		if i == 0 {
-			result += "0!"
-		} else {
-			result += string('0'+i)
-		}
-	}
+		result += string('0' + i)
+	} 
+	result += "!"
 	return result
 }
