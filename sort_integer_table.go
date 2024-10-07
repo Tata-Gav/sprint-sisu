1683e69fb4738889a90b3dac662aa11dadfcbdd9
Tata-Gav <tata.8web@gmail.com>
2024-10-07 16:35:10 +0300

    5

--- sort_integer_table.go
+++ sort_integer_table.go
@@ -1,14 +1,8 @@
 package sprint
-
+ 
 import "sort"
 
 func SortIntegerTable(table []int) []int {
-    // Create a copy of the original table to avoid modifying it in place
-    sortedTable := make([]int, len(table))
-    copy(sortedTable, table)
-
-    // Sort the copied table using the built-in sort function
-    sort.Ints(sortedTable)
-
-    return sortedTable
+     sort.Ints(table)
+	 return table
 }
