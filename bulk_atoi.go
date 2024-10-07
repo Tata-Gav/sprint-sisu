083703bfde95e19b477bb0cca76b7cad80fc7775
Tata-Gav <tata.8web@gmail.com>
2024-10-07 22:34:32 +0300

    6

--- bulk_atoi.go
+++ bulk_atoi.go
@@ -1,33 +1,33 @@
 package sprint
 
-func SimpleStrToInt(arr []string) []int {
-	
-	result := 0
-    i := 0
-    for {
-        if i >= len(s) {
-       break
+func ConvertStrToInt(s string) int {
+	if s == "" {
+        return 0
     }
     
-    char := s[i]
-    if char >=  '0' && char <= '9' {
-        digit := int (char - '0')
-        result = result*10 + digit
-        }
-        i++
+    isNegative := false
+    if s[0] == '-' {
+        isNegative = true
+        s = s[1:]
     }
-    
-    if len(s) > 0 && s[0] = '-' {
-        return -result
+    result := 0
+    for _, char := s {
+    if char < '0' || char > '9' {
+        return 0
     }
-    return result
-
+    result = result*10 + int(char-'0')
 }
+    if isNegative{
+    return -result
+}
+return result
+}
+
 func BulkAtoi(arr []string) []int {
     result := []int{}
-    for i := 0, i < len(arr); i++ {
-    value := SimpleStrToInt(arr[i])
-    result = append(result)
+
+    for _, str := range arr {
+        result = append(result, ConvertStrToInt(str))
     }
     return result 
 }
