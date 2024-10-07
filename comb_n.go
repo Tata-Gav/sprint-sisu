bf031649e10e79c2ac2e30dc8293519d2db0a0c7
Tata-Gav <tata.8web@gmail.com>
2024-10-07 23:04:03 +0300

    7

---
+++ comb_n.go
@@ -0,0 +1,32 @@
+package sprint
+
+import (
+	"fmt"
+)
+
+func CombN(n int) []string {
+	var results []string
+	combination := make ([]int, n)
+
+	var backtrack  func(start int, depth int)
+	backtrack = func (start int, depth int)  {
+		if depth  == n {
+			str := ""
+			for _, num := range combination {
+				str += fmt.Sprint(num)
+			}
+			result = append(result, str)
+			return
+			}
+			for i := start; i <= 9; i++ {
+				combination[depth] = i
+				backtrack(i+1, depth+1)
+			}
+			backtrack(0, 0)
+			return results
+		}
+		func main()  {
+			combination := CombN(3)
+			fmt.Println(combinations)	
+		}
+}
