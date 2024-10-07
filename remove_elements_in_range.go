0b546ad63150ca75de4b1f6a87d9c61d2bbdd7fa
Tata-Gav <tata.8web@gmail.com>
2024-10-07 14:31:35 +0300

    remove_elements_in_rangz.go

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -1,36 +1,37 @@
 package sprint
 
 func RemoveElementsInRange(arr []float64, from, to int) []float64 {
+    length := len(arr)
 
-	// Получаем длину массива
-	length := len(arr)
+    // Обработка отрицательных индексов
+    if from < 0 {
+        from += length
+    }
+    if to < 0 {
+        to += length
+    }
 
-	// Обрабатываем отрицательные индексы
-	if from < 0 {
-		from += length
-	}
-	if to < 0 {
-		to += length
-	}
+    // Убедимся, что индексы не выходят за пределы массива
+    if from < 0 {
+        from = 0
+    }
+    if from >= length {
+        from = length - 1
+    }
+    if to < 0 {
+        to = 0
+    }
+    if to >= length {
+        to = length - 1
+    }
 
-	// Корректируем индексы, если они выходят за пределы
-	if from >= length {
-		from = length - 1
-	}
-	if to >= length {
-		to = length - 1
-	}
-	if to < 0 {
-		to = 0
-	}
+    // Если индексы в неправильном порядке, поменяем их
+    if from > to {
+        from, to = to, from
+    }
 
-	// Если индексы в неправильном порядке, меняем их местами
-	if from > to {
-		from, to = to, from
-	}
+    // Удаляем элементы от from до to-1 включительно
+    result := append(arr[:from], arr[to:]...)
 
-	// Создаём новый массив, который включает элементы до from и после to
-	result := append(arr[:from], arr[to:]...)
-
-	return result
+    return result
 }
