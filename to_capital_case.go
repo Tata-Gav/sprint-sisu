4faae73172b433f929ee6509dd2a89fe7465cc0a
Tata-Gav <tata.8web@gmail.com>
2024-10-08 13:33:10 +0300

    9a

--- to_capital_case.go
+++ to_capital_case.go
@@ -1,35 +1,31 @@
-package Sprint
+package sprint
 
 func ToCapitalCase(s string) string {
     var result []rune
-    capitalizeNext := true // Flag to determine if the next character should be capitalized
+    capitalizeNext := true
 
     for i, r := range s {
         if isWordBoundary(byte(r)) {
-            result = append(result, r) // Append the word boundary character
-            capitalizeNext = true      // Next alphanumeric character should be capitalized
+            result = append(result, r)
+            capitalizeNext = true
         } else {
             if capitalizeNext {
-                // Capitalize the first letter of the word
                 if 'a' <= r && r <= 'z' {
                     r = r - ('a' - 'A')
                 }
-                capitalizeNext = false // Reset flag after capitalizing
+                capitalizeNext = false
             } else {
-                // Convert uppercase letters to lowercase for subsequent characters
                 if 'A' <= r && r <= 'Z' {
                     r = r + ('a' - 'A')
                 }
             }
-            result = append(result, r) // Append the modified character
+            result = append(result, r)
         }
     }
     return string(result)
 }
 
-// Function to check for word boundaries
 func isWordBoundary(c byte) bool {
     return c == ' ' || c == '!' || c == '?' || c == '[' || c == '{' || 
-           c == '(' || c == ':' || c == '}' || c == '-' || c == '/' || 
-           c == '+' || c == '%'
+           c == '(' || c == ':' || c == '}' ||
 }
