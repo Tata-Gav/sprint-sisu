3d821c393507b5ba7f444aebecd6a6c1026154f7
Tata-Gav <tata.8web@gmail.com>
2024-10-08 12:58:07 +0300

    10

--- to_capital_case.go
+++ to_capital_case.go
@@ -1,30 +1,23 @@
-package sprint
-
 func ToCapitalCase(s string) string {
-	result := ""
-	capitalizeNext := true // Флаг для определения, нужно ли капитализировать следующую букву
+    var result strings.Builder
+    capitalizeNext := true
 
-	for _, char := range s {
-		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9') {
-			if capitalizeNext {
-				if char >= 'a' && char <= 'z' {
-					char -= 32 // Преобразуем строчную букву в заглавную
-				}
-				result += string(char) // Добавляем заглавную букву
-				capitalizeNext = false  // Сбрасываем флаг
-			} else {
-				if char >= 'A' && char <= 'Z' {
-					char += 32 // Преобразуем заглавную букву в строчную
-				}
-				result += string(char) // Добавляем строчную букву
-			}
-		} else {
-			// Если символ не является буквой или цифрой, добавляем его без изменений
-			result += string(char)
-			if char == ' ' || char == '-' || char == '_' {
-				capitalizeNext = true // Устанавливаем флаг для следующего слова
-			}
-		}
-	}
-	return result // Возвращаем результат только после обработки всех символов
+    for _, r := range s {
+        if (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') {
+            if capitalizeNext {
+                r = r - 32
+            } else {
+                r = r + 32
+            }
+            result.WriteRune(r)
+            capitalizeNext = false
+        } else if r == ' ' || r == '-' || r == '_' {
+            result.WriteRune(r)
+            capitalizeNext = true
+        } else {
+            result.WriteRune(r)
+        }
+    }
+
+    return result.String()
 }
