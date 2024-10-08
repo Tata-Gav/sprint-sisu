3902eb03ae94a708f31522f80bb6e20385969d06
Tata-Gav <tata.8web@gmail.com>
2024-10-08 10:34:50 +0300

    5

--- str_reverse.go
+++ str_reverse.go
@@ -2,8 +2,8 @@ package sprint
 
 func StrReverse(s string) string {
 	runes := []rune(s) //Преобразуем строку в срез символов
-	for i, j := 0, len(rune)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes [j], runes[i]
 	}
-	returnstring(runes)
+	return string(runes)
 }
