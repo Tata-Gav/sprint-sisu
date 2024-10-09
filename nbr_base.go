31c250cb6b23fb5cbf67274494de9176d206c645
Tata-Gav <tata.8web@gmail.com>
2024-10-09 08:36:56 +0300

    15

---
+++ nbr_base.go
@@ -0,0 +1,28 @@
+package sprint
+
+func NbrBase(n int, base string) string {
+	if len(base) < 2 || strings.ContainsAny(base, "+-") {
+		return "NV"
+	}
+
+	isNegative := n < 0
+	n = int(math.Abs(float64(n)))
+
+	baseMap := make(map[int]rune)
+	for i, char := range base {
+		baseMap[i] = char
+	}
+
+	result := ""
+	for n > 0 {
+		remainder := n % len(base)
+		result = string(baseMap[remainder]) + result
+		n /= len(base)
+	}
+
+	if isNegative {
+		result = "-" + result
+	}
+
+	return result
+}
