6e8a6fa373510897abb75831936e1fe80c083dbf
Tata-Gav <tata.8web@gmail.com>
2024-10-09 11:54:14 +0300

    16

---
+++ convert_any_to_dec.go
@@ -0,0 +1,28 @@
+package sprint
+
+import (
+	"math"
+	"strings"
+)
+
+func ConvertAnyToDec(s string, base string) int {
+	if len(base) < 2 || strings.ContainsAny(base, "+-") {
+			return 0
+	}
+
+	baseMap := make(map[rune]int)
+	for i, char := range base {
+			baseMap[char] = i
+	}
+
+	result := 0
+	baseLen := len(base)
+	for _, char := range s {
+			if _, ok := baseMap[char]; !ok {
+					return 0
+			}
+			result = result*baseLen + baseMap[char]
+	}
+
+	return result
+}
