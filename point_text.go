7025c12aee8c56697f5a75111d0fc8d0cc3ca63d
Tata-Gav <tata.8web@gmail.com>
2024-10-10 10:33:44 +0300

    3f

---
+++ point_text.go
@@ -0,0 +1,19 @@
+package sprint
+
+import (
+	"fmt"
+)
+
+type Point struct {
+	X float32
+	Y float32
+	Text string
+}
+
+func PointText(p Point) Point {
+	return Point{
+		X: p.X,
+		Y: p.Y,
+		Text: fmt.Sprintf("Text at (%.2f, %.2f)", p.X, p.Y),
+	}
+}
