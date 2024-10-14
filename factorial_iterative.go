f5968687c631901d6938699da9238f20b9a17f07
Tata-Gav <tata.8web@gmail.com>
2024-10-14 11:52:42 +0300

    1

--- factorial_iterative.go
+++ factorial_iterative.go
@@ -1,17 +1,19 @@
 package sprint
 
+import "math"
+
 func FactorialIterative(n int) int {
-	if n < 0 {
-		return 0 // Handle negative numbers
-	}
-	// just for checking yes
-	result := 1
-	for i := 1; i <= n; i++ {
-		// Check for overflow before multiplying
-		if result > int(^uint(0))/i {
-			return 0 // Return 0 for overflow
-		}
-		result *= i + 1
-	}
-	return result
+    if n < 0 {
+        return 0 // Handle negative numbers
+    }
+
+    result := 1
+    for i := 1; i <= n; i++ {
+        // Check for overflow before multiplying
+        if result > math.MaxInt64/i {
+            return 0 // Return 0 for overflow
+        }
+        result *= i
+    }
+    return result
 }
