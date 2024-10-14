320660db91437d13e7f461c951618828dbd8d503
Tata-Gav <tata.8web@gmail.com>
2024-10-14 13:09:03 +0300

    2

--- factorial_recursive.go
+++ factorial_recursive.go
@@ -1,19 +1,18 @@
 package sprint
 
+import (
+    "math"
+)
 func FactorialRecursive(n int) int {
     if n < 0 {
-        return 0 // Handle negative numbers
+        return 0 // non-possible value
     }
-
     if n == 0 || n == 1 {
         return 1
     }
-
-    // Check for overflow before multiplying
-    result := FactorialRecursive(n-1)
-    if n*result < 0 {
-        return 0 // Return 0 for overflow
+    result := n * FactorialRecursive(n-1)
+    if result < 0 || result > math.MaxInt { // Check for overflow
+        return 0
     }
-
-    return n * result
+    return result
 }
