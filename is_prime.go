d9d6fe63bcfa56595e75e58034b0c053f58acaa9
Tata-Gav <tata.8web@gmail.com>
2024-10-14 13:43:08 +0300

    6

--- is_prime.go
+++ is_prime.go
@@ -1,14 +1,12 @@
 package sprint
 
-import "math"
-
 func IsPrime(n int) bool {
     if n <= 1 {
         return false // Handle numbers less than or equal to 1
     }
 
     // Check divisibility up to the square root of n
-    for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+    for i := 2; i*i <= n; i++ {
         if n%i == 0 {
             return false // Not prime if divisible
         }
