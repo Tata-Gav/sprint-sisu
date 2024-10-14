c8f93950ea4c884902ae31868eff1d36c350f734
Tata-Gav <tata.8web@gmail.com>
2024-10-14 13:37:20 +0300

    5

---
+++ nth_fibonacci.go
@@ -0,0 +1,17 @@
+package sprint
+
+unc NthFibonacci(index int) int {
+    if index < 0 {
+        return -1 // Handle negative indices
+    }
+
+    if index == 0 {
+        return 0
+    }
+
+    if index == 1 {
+        return 1
+    }
+
+    return NthFibonacci(index-1) + NthFibonacci(index-2)
+}
