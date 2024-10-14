9fa04b4b7f375c0107c4cdc5597bfc062e7ce294
Tata-Gav <tata.8web@gmail.com>
2024-10-14 10:34:59 +0300

    new1

---
+++ sprint/factorial_iterative.go
@@ -0,0 +1,16 @@
+package sprint
+func FactorialIterative(n int) int {
+    if n < 0 {
+        return 0 // Handle negative numbers
+    }
+
+    result := 1
+    for i := 1; i <= n; i++ {
+        // Check for overflow before multiplying
+        if result > int(^uint(0))/i {
+            return 0 // Return 0 for overflow
+        }
+        result *= i
+    }
+    return result
+}
