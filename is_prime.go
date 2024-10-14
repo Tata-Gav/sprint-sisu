69307234cfff1d25bea108c68bae505adb072814
Tata-Gav <tata.8web@gmail.com>
2024-10-14 13:41:12 +0300

    6

---
+++ is_prime.go
@@ -0,0 +1,16 @@
+package sprint
+
+func IsPrime(n int) bool {
+    if n <= 1 {
+        return false // Handle numbers less than or equal to 1
+    }
+
+    // Check divisibility up to the square root of n
+    for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+        if n%i == 0 {
+            return false // Not prime if divisible
+        }
+    }
+
+    return true // Prime if no divisors found
+}
