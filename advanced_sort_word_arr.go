2f6829439cce898934464038c85d0de1798e0ff6
Tata-Gav <tata.8web@gmail.com>
2024-10-15 10:52:19 +0300

    4

---
+++ advanced_sort_word_arr.go
@@ -0,0 +1,20 @@
+package sprint
+
+import (
+        "strings"
+)
+
+func StrCompare(a, b string) int {
+        return strings.Compare(a, b)
+}
+
+func AdvancedSortWordArr(a []string, f func(a, b string) int) []string {
+        for i := 0; i < len(a)-1; i++ {
+                for j := i + 1; j < len(a); j++ {
+                        if f(a[i], a[j]) > 0 {
+                                a[i], a[j] = a[j], a[i]
+                        }
+                }
+        }
+        return a
+}
