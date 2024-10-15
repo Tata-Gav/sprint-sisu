a04ef1fd22dc5ed63dfa100ce6d4c81d393ac725
Tata-Gav <tata.8web@gmail.com>
2024-10-15 11:09:23 +0300

    4

--- advanced_sort_word_arr.go
+++ advanced_sort_word_arr.go
@@ -1,18 +1,19 @@
-package sprint
+package main
 
-// StrCompare function should be defined beforehand
+import "strings"
+
 func StrCompare(a, b string) int {
-    if a < b {
-        return -1
-    } else if a > b {
-        return 1
-    }
-    return 0
+    return strings.Compare(a, b)
 }
 
 func AdvancedSortWordArr(a []string, f func(a, b string) int) []string {
-    sort.Slice(a, func(i, j int) bool {
-        return f(a[i], a[j]) < 0
-    })
+    // Implementation using bubble sort for simplicity
+    for i := 0; i < len(a)-1; i++ {
+        for j := i + 1; j < len(a); j++ {
+            if f(a[i], a[j]) > 0 {
+                a[i], a[j] = a[j], a[i]
+            }
+        }
+    }
     return a
 }
