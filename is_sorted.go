efbe31f70f3800d3db3729a959d96d2fafe7da0c
Tata-Gav <tata.8web@gmail.com>
2024-10-15 11:25:43 +0300

    5

--- is_sorted.go
+++ is_sorted.go
@@ -1,9 +1,22 @@
 package sprint
 
-import "strings"
 
 func StrCompare(a, b string) int {
-    return strings.Compare(a, b)
+    for i := 0; i < len(a) && i < len(b); i++ {
+        if a[i] < b[i] {
+            return -1
+        } else if a[i] > b[i] {
+            return 1
+        }
+    }
+
+    if len(a) < len(b) {
+        return -1
+    } else if len(a) > len(b) {
+        return 1
+    }
+
+    return 0
 }
 
 func IsSorted(f func(a, b string) int, arr []string) bool {
