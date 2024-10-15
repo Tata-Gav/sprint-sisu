6830a5d38ced315ac4dbf617f04aef12b7e11032
Tata-Gav <tata.8web@gmail.com>
2024-10-15 10:04:20 +0300

    1

--- arr_any.go
+++ arr_any.go
@@ -1,32 +1,28 @@
 package sprint
 
-import (
-	"unicode"
-)
-
-func ArrAny(f func(string) bool, a []string) bool {
-	for _, s := range a {
-			if f(s) {
-					return true
-			}
-	}
-	return false
-}
-
 func IsUpper(s string) bool {
-	for _, c := range s {
-			if !unicode.IsUpper(c) {
-					return false
-			}
-	}
-	return true
+    for _, c := range s {
+        if c >= 'a' && c <= 'z' {
+            return false
+        }
+    }
+    return true
 }
 
 func IsAlphanumeric(s string) bool {
-	for _, c := range s {
-			if !unicode.IsLetter(c) && !unicode.IsDigit(c) {
-					return false
-			}
-	}
-	return true
+    for _, c := range s {
+        if !(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9')) {
+            return false
+        }
+    }
+    return true
+}
+
+func ArrAny(f func(string) bool, a []string) bool {
+    for _, s := range a {
+        if f(s) {
+            return true
+        }
+    }
+    return false
 }
