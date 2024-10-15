a7accf734a8ae0d08b4ab8b7a603e541cf79224a
Tata-Gav <tata.8web@gmail.com>
2024-10-15 10:13:18 +0300

    1

--- arr_any.go
+++ arr_any.go
@@ -1,32 +1,46 @@
 package sprint
 
-func IsUpper(s string) bool {
+// IsLower проверяет, содержит ли строка только строчные символы
+func IsLower(s string) bool {
     for _, c := range s {
-        if c >= 'a' && c <= 'z' {
+        if c < 'a' || c > 'z' { // Если символ не в диапазоне строчных букв
             return false
         }
     }
-    return true
+    return len(s) > 0 // Возвращаем true, если строка не пустая и все символы строчные
 }
 
+// IsUpper проверяет, содержит ли строка только заглавные буквы
+func IsUpper(s string) bool {
+    for _, c := range s {
+        if c >= 'a' && c <= 'z' { // Если находим строчный символ, возвращаем false
+            return false
+        }
+    }
+    return len(s) > 0 // Возвращаем true, если строка не пустая и все символы заглавные
+}
+
+// IsAlphanumeric проверяет, содержит ли строка только алфавитные и числовые символы
 func IsAlphanumeric(s string) bool {
     for _, c := range s {
         if !(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9')) {
             return false
         }
     }
-    return true
+    return len(s) > 0 // Возвращаем true, если строка не пустая и содержит только буквы/цифры
 }
 
+// IsNumeric проверяет, содержит ли строка только цифры
 func IsNumeric(s string) bool {
     for _, c := range s {
-        if !('0' <= c && c <= '9') {
+        if !('0' <= c && c <= '9') { // Если находим нецифровой символ, возвращаем false
             return false
         }
     }
-    return true
+    return len(s) > 0 // Возвращаем true, если строка не пустая и содержит только цифры
 }
 
+// ArrAny возвращает true, если хотя бы один элемент среза a возвращает true для функции f
 func ArrAny(f func(string) bool, a []string) bool {
     for _, s := range a {
         if f(s) {
