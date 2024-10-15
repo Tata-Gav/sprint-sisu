d633db415abb7d6b65eb9310bacbc9ef7901dd01
Tata-Gav <tata.8web@gmail.com>
2024-10-15 11:15:32 +0300

    4

--- advanced_sort_word_arr.go
+++ advanced_sort_word_arr.go
@@ -17,3 +17,15 @@ func StrCompare(a, b string) int {
 
     return 0
 }
+
+func AdvancedSortWordArr(a []string, f func(a, b string) int) []string {
+    // Implementation using bubble sort for simplicity
+    for i := 0; i < len(a)-1; i++ {
+        for j := i + 1; j < len(a); j++ {
+            if f(a[i], a[j]) > 0 {
+                a[i], a[j] = a[j], a[i]
+            }
+        }
+    }
+    return a
+}
