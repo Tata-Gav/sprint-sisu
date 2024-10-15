ec73d7d05eccebb0f5f2b00d9e61cd7eded8d929
Tata-Gav <tata.8web@gmail.com>
2024-10-15 10:57:14 +0300

    4

--- advanced_sort_word_arr.go
+++ advanced_sort_word_arr.go
@@ -1,16 +1,22 @@
 package sprint
+import (
+    "fmt"
+    "sort"
+)
 
+// StrCompare function should be defined beforehand
 func StrCompare(a, b string) int {
-	return strings.Compare(a, b)
+    if a < b {
+        return -1
+    } else if a > b {
+        return 1
+    }
+    return 0
 }
 
 func AdvancedSortWordArr(a []string, f func(a, b string) int) []string {
-	for i := 0; i < len(a)-1; i++ {
-			for j := i + 1; j < len(a); j++ {
-					if f(a[i], a[j]) > 0 {
-							a[i], a[j] = a[j], a[i]
-					}
-			}
-	}
-	return a
+    sort.Slice(a, func(i, j int) bool {
+        return f(a[i], a[j]) < 0
+    })
+    return a
 }
