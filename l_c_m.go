6c79ed07fae600ede0f7ce77be32e1ef1ce9e750
Tata-Gav <tata.8web@gmail.com>
2024-10-16 12:24:10 +0300

    2

---
+++ l_c_m.go
@@ -0,0 +1,19 @@
+package sprint
+
+func GSD(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+
+}
+func LCM(a, b int) int {
+	return abs(a * b) / GCD(a, b)
+
+}
+ funk abc(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+ }
