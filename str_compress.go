c0da94a3a56a9e161926c7e06bd2b2865f2c1d03
Tata-Gav <tata.8web@gmail.com>
2024-10-16 13:54:23 +0300

    6

---
+++ str_compress.go
@@ -0,0 +1,27 @@
+package sprint
+
+func StrCompress(input string) string {
+    var compressed string
+    count := 1
+
+    for i := 1; i < len(input); i++ {
+        if input[i] == input[i-1] {
+            count++
+        } else {
+            if count > 1 {
+                compressed += strconv.Itoa(count) + string(input[i-1])
+            } else {
+                compressed += string(input[i-1])
+            }
+            count = 1
+        }
+    }
+
+    if count > 1 {
+        compressed += strconv.Itoa(count) + string(input[len(input)-1])
+    } else {
+        compressed += string(input[len(input)-1])
+    }
+
+    return compressed
+}
