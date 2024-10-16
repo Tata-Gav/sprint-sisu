8a331bcf39cb85664fd4e4271b1fd5c64ede32f4
Tata-Gav <tata.8web@gmail.com>
2024-10-16 13:02:25 +0300

    5

--- sort_word_arr.go
+++ sort_word_arr.go
@@ -1,10 +1,30 @@
 package sprint
 
-import (
-"sort"
-)
+func StrCompare(a, b string) int {
+    for i := 0; i < len(a) && i < len(b); i++ {
+        if a[i] < b[i] {
+            return -1
+        } else if a[i] > b[i] {
+            return 1
+        }
+    }
+
+    if len(a) < len(b) {
+        return -1
+    } else if len(a) > len(b) {
+        return 1
+    }
+
+    return 0
+}
 
 func SortWordArr(a []string) []string {
-    sort.Strings(a)
+    for i := 0; i < len(a)-1; i++ {
+        for j := i + 1; j < len(a); j++ {
+            if StrCompare(a[i], a[j]) > 0 {
+                a[i], a[j] = a[j], a[i]
+            }
+        }
+    }
     return a
 }
