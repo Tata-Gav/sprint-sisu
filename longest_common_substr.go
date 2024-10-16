35cf30801198b21047b0d658b563a2add9375907
Tata-Gav <tata.8web@gmail.com>
2024-10-16 16:00:20 +0300

    12

--- longest_common_substr.go
+++ longest_common_substr.go
@@ -1,19 +1,19 @@
 package sprint
 
 func LongestCommonSubstr(str1, str2 string) string {
-	m := len(str1)
-	n := len(str2)
-	maxLenght := 0
-	endingIndex := 0
+    m := len(str1)
+    n := len(str2)
+    maxLength := 0
+    endingIndex := 0
 
-	lcsuff := make([][]int, m+1)
-	for i := range lcsuff {
-		lcsuff[i] = make([]int, n+1)
-	}
+    lcsuff := make([][]int, m+1)
+    for i := range lcsuff {
+        lcsuff[i] = make([]int, n+1)
+    }
 
-	for i := 1; i <= m; i++ {
-		for j := 1; j <= n; j++ {
-			if str1[i-1] == str2[j-1]{
+    for i := 1; i <= m; i++ {
+        for j := 1; j <= n; j++ {
+            if str1[i-1] == str2[j-1] {
                 lcsuff[i][j] = lcsuff[i-1][j-1] + 1
                 if lcsuff[i][j] > maxLength {
                     maxLength = lcsuff[i][j]
@@ -28,4 +28,3 @@ func LongestCommonSubstr(str1, str2 string) string {
     }
 
     return str1[endingIndex-maxLength : endingIndex]
-}
